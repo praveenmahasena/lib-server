46acd06d2f9aaba88b4442efbaec270feb9b9c02
praveenmahasena <praveenmahasena647@gmail.com>
2025-02-06 13:21:46 -0500

    adding initial setup

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/praveenmahasena/server
 
 go 1.23.6
+
+require (
+	github.com/BurntSushi/toml v1.4.1-0.20240526193622-a339e1f7089c // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20250128182459-e0ece0dbea4c // indirect
+	golang.org/x/mod v0.23.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/tools v0.29.0 // indirect
+	honnef.co/go/tools v0.5.1 // indirect
+)
--- internal/app.go
+++ internal/app.go
@@ -1,3 +1,47 @@
 package internal
 
-func Run() error { return nil }
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/joho/godotenv"
+	db "github.com/praveenmahasena/server/internal/DB"
+	"github.com/praveenmahasena/server/internal/server"
+)
+var S *server.Server
+
+func Run() error {
+	if dotEnvErr := godotenv.Load(".env"); dotEnvErr != nil {
+		return fmt.Errorf("error during reading .env file %v", dotEnvErr)
+	}
+	libConnectionUri := db.LibDBUri()
+	connection, connectionErr := db.NewConnection(libConnectionUri)
+	if connectionErr != nil {
+		return connectionErr
+	}
+	S = server.New(":42069", connection)
+	errCh := make(chan error)
+	sig := make(chan os.Signal, 1)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel() // I do not like it here but I don't wanna have accidental context leak :)
+	go func(e chan<- error) {
+		e <- S.Start(ctx)
+	}(errCh)
+
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	// The errCh returns the val here if it has problem starting up the server
+	case err := <-errCh:
+		return err
+	case <-sig:
+		cancel()
+		fmt.Fprintln(os.Stdout, "shutting down server...")
+	}
+	// The errCh returns the val here if it has problem closing up the server
+	return <-errCh
+}
